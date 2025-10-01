f65e333132a0cfb62ce9e24e9e47d3c32e4e2d91
“Programador753” <antoniohcavero@outlook.com>
2025-10-01 11:15:41 +0200

    actualizo

--- Biblioteca/src/java/Controlador/AutorController.java
+++ Biblioteca/src/java/Controlador/AutorController.java
@@ -47,7 +47,7 @@ public class AutorController implements Serializable {
 
     public PaginationHelper getPagination() {
         if (pagination == null) {
-            pagination = new PaginationHelper(10) {
+            pagination = new PaginationHelper(getItemsAvailableSelectOne().length) {
 
                 @Override
                 public int getItemsCount() {
